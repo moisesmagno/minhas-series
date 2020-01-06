f20018d3b30496ec1b9c61041e0095dc68bf36a0
Moisés S E Aguilar <moisesmagno@gmail.com>
2020-01-06 09:53:29 -0300

    Concluido o mini projeto de Minhas séries.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./Header";
+import Home from "./Home";
 import Series from "./Series";
 import NovaSerie from "./NovaSerie";
 import InfoSerie from "./InfoSerie";
 import Generos from "./Generos";
 import NovoGenero from "./NovoGenero";
 import EditarGenero from "./EditarGenero";
-
-const Home = () => <h1> Home </h1>;
 
 function App() {
   return (
